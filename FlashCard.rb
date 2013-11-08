02b9ad192c45508fd8378a7db99402dfa056fa8a
4RN4LDO <arnaldoporras@gmail.com>
2013-11-08 13:16:00 -0600

    process

--- FlashCard.rb
+++ FlashCard.rb
@@ -2,8 +2,10 @@ words = %w( hola adios perro gato carro casa hermano primo)
 print "Please translate this word to spanish 'hello' \n" 
 input = gets.chomp
 
-if input == words[0]
-	puts "that's correct"
-else
-	puts "sorry, that's not correct"
+loop do
+	if input == words[0]
+		puts "that's correct"
+	else
+		puts "sorry, that's not correct"
+	end
 end
