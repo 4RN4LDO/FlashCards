419ba1432f56ace9d577f814f1ebbaa57572d6d7
4RN4LDO <arnaldoporras@gmail.com>
2013-11-08 15:51:11 -0600

    process

--- FlashCard.rb
+++ FlashCard.rb
@@ -4,10 +4,21 @@ loop do
 print "Please translate this word to spanish 'hello' \n" 
 write= gets.chomp
 	unless write == words[0]
-		puts "sorry, that's not correct"
+		puts "That's not correct! you write: #{write}, the correct word was 'hola'!"
+		puts "Let's try another one"
+	else
+		puts "that's correct!"
+		#exit
+	end
+
+print "Now let's try this one 'bye' \n" 
+write= gets.chomp
+	unless write == words[1]
+		puts "That's not correct! you write: #{write}, the correct word was 'adios'!"
+		puts "Let's try another one"
 		exit
 	else
-		puts "that's correct"
+		puts "that's correct!"
 		exit
-	end
+	end	
 end
