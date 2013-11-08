7bfbebb3c8fed6e2396cf832142278a9ab386ae9
4RN4LDO <arnaldoporras@gmail.com>
2013-11-08 13:26:13 -0600

    process

--- FlashCard.rb
+++ FlashCard.rb
@@ -1,13 +1,13 @@
 words = %w( hola adios perro gato carro casa hermano primo)
 
 loop do
-	print "Please translate this word to spanish 'hello' \n" 
-	input = gets.chomp
-	unless input == words[0]
-		puts "that's correct"
+print "Please translate this word to spanish 'hello' \n" 
+write= gets.chomp
+	unless write == words[0]
+		puts "sorry, that's not correct"
 		exit
 	else
-		puts "sorry, that's not correct"
+		puts "that's correct"
 		exit
 	end
 end
