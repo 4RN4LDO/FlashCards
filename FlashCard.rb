ea9004fa7346c45696bc71513fea8afa8037db6d
4RN4LDO <arnaldoporras@gmail.com>
2013-11-08 16:39:06 -0600

    process

--- FlashCard.rb
+++ FlashCard.rb
@@ -1,24 +1,30 @@
 spanishWords = %w( hola adios perro gato carro casa hermano primo)
 englishWords = %w( hello bye dog cat car house brother cousin )
+score = 0
+
+puts "Welcome, let's learn some spanish"
+puts "Translate: "
 
 loop do
-print "Please translate " + englishWords[0] + " to spanish \n" 
+print englishWords[0] + "\n" 
 write= gets.chomp
 	unless write == spanishWords[0]
 		puts "That's not correct! you write: #{write}, the correct word was 'hola'!"
 		puts "Let's try another one"
 	else
+		score += 1
 		puts "that's correct!"
 		#exit
 	end
 
-print "Now let's try this one 'bye' \n" 
+print englishWords[1] + "\n" 
 write= gets.chomp
 	unless write == spanishWords[1]
 		puts "That's not correct! you write: #{write}, the correct word was 'adios'!"
 		puts "Let's try another one"
 		exit
 	else
+		score += 1
 		puts "that's correct!"
 		exit
 	end	
