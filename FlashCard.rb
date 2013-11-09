f163d5079906962e14fb7db021fcbd57ab5ad286
4RN4LDO <arnaldoporras@gmail.com>
2013-11-09 17:31:42 -0600

    process

--- FlashCard.rb
+++ FlashCard.rb
@@ -20,7 +20,7 @@ print englishWords[1] + "\n"
 write= gets.chomp
 	unless write == spanishWords[1]
 		puts "Ups! you write: #{write}, the correct word was '#{spanishWords[1]}'!"
-		puts "Let's try another one"
+		puts "Here is another one"
 	else
 		score += 1
 		puts "nice!"
@@ -30,7 +30,7 @@ print englishWords[2] + "\n"
 write= gets.chomp
 	unless write == spanishWords[2]
 		puts "Come on! you can do better you put: #{write}, and the correct word was '#{spanishWords[2]}'!"
-		puts "Let's try another one"
+		puts "Let's do it again"
 	else
 		score += 1
 		puts "cool!"
@@ -40,7 +40,7 @@ print englishWords[3] + "\n"
 write= gets.chomp
 	unless write == spanishWords[3]
 		puts "It's just a mistake! you write: #{write}, the correct was '#{spanishWords[3]}'!"
-		puts "Let's try another one"
+		puts "Here is a new one"
 	else
 		score += 1
 		puts "WOW!"
@@ -50,7 +50,7 @@ print englishWords[4] + "\n"
 write= gets.chomp
 	unless write == spanishWords[4]
 		puts "Sorry, you write: #{write}, and the correct word was '#{spanishWords[4]}'!"
-		puts "Let's try another one"
+		puts "Try with this one"
 	else
 		score += 1
 		puts "Keep it that way!"
@@ -60,10 +60,10 @@ print englishWords[5] + "\n"
 write= gets.chomp
 	unless write == spanishWords[5]
 		puts "You are learning! you write: #{write}, the correct word was '#{spanishWords[5]}'!"
-		puts "Let's try another one"
+		puts "This one is easy "
 	else
 		score += 1
-		puts "Who is learning? you!"
+		puts "Now, who is learning? you!"
 	end	
 
 print englishWords[6] + "\n" 
@@ -82,7 +82,7 @@ write= gets.chomp
 		puts "That's not correct! you write: #{write}, the correct word was '#{spanishWords[7]}'!"
 	else
 		score += 1
-		puts "that's correct!"
+		puts "Yei!"
 	end	
 
 	if score == 1
